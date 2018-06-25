d1b8527549f8beadbd6e6e84c0fb3450f106c831
r <r@localhost>
2018-06-25 08:28:13 +0200

    use console.error for errors in httpclient.js

--- learnyounode/httpclient.js
+++ learnyounode/httpclient.js
@@ -4,5 +4,5 @@ var url = process.argv[2];
 http.get(url, function (response) {
   response.setEncoding('utf8');
   response.on('data', (data) => { console.log(data); });
-  response.on('error', (error) => { console.log(error); });
+  response.on('error', (error) => { console.error(error); });
 }).on('error', console.error);
